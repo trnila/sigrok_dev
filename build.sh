aa8c7ef5d6f6eb6902aa6c033d1f039bd1a7ec35
Daniel Trnka <daniel.trnka@gmail.com>
2024-08-30 20:59:00 +0200

    Do not reconfigure cmake for pulseview

--- build.sh
+++ build.sh
@@ -12,8 +12,10 @@ build() (
   cd "$1"
 
   if [ "$1" = "pulseview" ]; then
-    export PKG_CONFIG_PATH
-    cmake . -DCMAKE_INSTALL_PREFIX:string=$PREFIX -DENABLE_TESTS=n
+    if [ ! -d CMakeFiles ]; then
+        export PKG_CONFIG_PATH
+        cmake . -DCMAKE_INSTALL_PREFIX:string=$PREFIX -DENABLE_TESTS=n
+    fi
   elif [ ! -f ./configure ]; then
     ./autogen.sh
     ./configure --prefix=$PREFIX PKG_CONFIG_PATH=$PKG_CONFIG_PATH --enable-java=0 --enable-ruby=0
