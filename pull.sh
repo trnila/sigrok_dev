f06757b0c232a7bea5286d9d4e5633f622bde975
Daniel Trnka <daniel.trnka@gmail.com>
2024-09-18 00:40:19 +0200

    pull.sh: just pull all gits

--- pull.sh
+++ pull.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 set -ex
 
-repos="libsigrok libsigrokdecode sigrok-cli pulseview"
-
-for repo in $repos; do
-  if [ ! -d "$repo" ]; then
-    git clone "git://sigrok.org/$repo"
-  else
-    (cd "$repo" && git pull)
-  fi
-done
-
+find ./* -name .git | while read -r repo; do (
+	cd "$repo"/..
+	git pull
+) done
